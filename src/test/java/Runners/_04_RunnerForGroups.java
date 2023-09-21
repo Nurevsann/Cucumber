a2d2bb69a6ecf71e85e884af1087a69f70279892
nurev <nurevsankurak@gmail.com>
2023-09-21 21:02:29 +0300

    add new test scnerio

---
+++ src/test/java/Runners/_04_RunnerForGroups.java
@@ -0,0 +1,14 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/java/FeatureFiles",
+        tags = "@Smoke",
+        glue = "StepDefinitions",
+        plugin = {"json:target/cucumber.json"}
+)
+public class _04_RunnerForGroups extends AbstractTestNGCucumberTests {
+}
